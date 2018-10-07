9f20e5ab7e8e431d51ea9cb3ded5b0f9b8ec99d6
Jacob Honoré <643471+JacobHonore@users.noreply.github.com>
2018-10-07 22:07:17 +0200

    Handle change of business and location boxes

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -13,6 +13,8 @@ class SearchBar extends React.Component {
       location: '',
       sortBy: 'best_match'
     };
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
   }
   renderSortByOptions() {
     return Object.keys(sortByOptions).map(sortByOption => {
@@ -30,11 +32,19 @@ class SearchBar extends React.Component {
     return '';
   }
   handleSortByChange(sortByOption) {
-    this.setState(
-      {
-        sortBy: sortByOption
-      }
-    );
+    this.setState({
+      sortBy: sortByOption
+    });
+  }
+  handleTermChange(event) {
+    this.setState({
+      term: event.target.value
+    });
+  }
+  handleLocationChange(event) {
+    this.setState({
+      location: event.target.value
+    });
   }
   render() {
     return (
@@ -45,8 +55,8 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+          <input placeholder="Where?" onChange={this.handleLocationChange} />
         </div>
         <div className="SearchBar-submit">
           <a>Lets Go</a>
